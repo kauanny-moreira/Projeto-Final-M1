2f43eb671101215cb5ec75bd63edd92936c8621e
kauanny-moreira <kauannymoreira82@gmail.com>
2025-02-26 00:01:31 -0300

    Alteração feita por Agatha Lauanda: conclusão do carrinho

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-const botaoht = document.getElementById("botao");
-const body = document.body;
-
-botaoht.addEventListener("click", function(){
-  body.classList.toggle("modoescuro")
-})
-
-
-var listaProdutos = [
-      { img: "hamburguer1.png", titulo: "Marquês de Beef", descricao: "Carne bovina envolvida em massa folhada, acompanhada de molho madeira e purê de batata.", preco: "Preço: R$40,00" },
-      { img: "hamburguer2.png", titulo: "Nova York Classic", descricao: " Clássico americano com queijo cheddar, bacon crocante e cebola caramelizada em pão brioche.", preco: "Preço: R$50,00" },
-      { img: "hamburguer3.png", titulo: "Toscana Trufado", descricao: "Hambúrguer de carne bovina com queijo gorgonzola, trufas negras e rúcula, em pão artesanal.", preco:"Preço: R$45,00"},
-      { img: "hamburguer4.png", titulo: "Mediterrâneo", descricao: "Hambúrguer de cordeiro com molho tzatziki, tomate seco e cebola roxa em pão pita.", preco: "Preço: R$45,00" },
-      { img: "hamburguer5.png", titulo: "Vegetariano", descricao: "Pão, hambúrguer de grão de bico, homus, tomate seco e rúcula.", preco: "Preço: R$40,00" },
-      { img: "hamburguer6.png", titulo:"Cogumelos Trufados", descricao: "Pão, hambúrguer de cogumelos, queijo brie, azeite trufado e rúcula.", preco: "Preço: R$50,00" },
-  ]
-  
-  
-  function exibirLista() {
-      for (var i = 0; i < listaProdutos.length; i++) {
-          document.getElementById("produtos").innerHTML+=`<li class="card">
-              <div  class="divphoto"><img class="imgproduct" src="${listaProdutos[i].img}"></div>    
-              <h3  class="nomeproduto">${listaProdutos[i].titulo}</h3>
-              <p class="descricao">${listaProdutos[i].descricao} </p>
-              <p class="descricao-preco" > <strong>${listaProdutos[i].preco} </strong></p>
-              <button id= "botao" >Adicionar ao Carrinho</button>
-              </li>
-              `
-      }
-  }
-  
-  exibirLista();
-
-
-  let nome = prompt("Qual é o seu nome?");
+let nome = prompt("Qual é o seu nome?");
 alert("Seja bem vindo(a) ao 'Big Bite', " + nome + "!");
 
 
+const botaoht = document.getElementById("botao");
+const body = document.body;
+
+botaoht.addEventListener('click', () => {
+    body.classList.toggle('modoescuro');
+});
+
+var listaProdutos = [
+    { id:1, img: "hamburguer1.png", titulo: "Marquês de Beef", descricao: "Carne bovina envolvida em massa folhada, acompanhada de molho madeira e purê de batata.", preco: "40,00" },
+    { id:2, img: "hamburguer2.png", titulo: "Nova York Classic", descricao: " Clássico americano com queijo cheddar, bacon crocante e cebola caramelizada em pão brioche.", preco: "50,00" },
+    { id:3, img: "hamburguer3.png", titulo: "Toscana Trufado", descricao: "Hambúrguer de carne bovina com queijo gorgonzola, trufas negras e rúcula, em pão artesanal.", preco:"45,00"},
+    { id:4, img: "hamburguer4.png", titulo: "Mediterrâneo", descricao: "Hambúrguer de cordeiro com molho tzatziki, tomate seco e cebola roxa em pão pita.", preco: "45,00" },
+    { id:5, img: "hamburguer5.png", titulo: "Vegetariano", descricao: "Pão, hambúrguer de grão de bico, homus, tomate seco e rúcula.", preco: "40,00" },
+    { id:6, img: "hamburguer6.png", titulo:"Cogumelos Trufados", descricao: "Pão, hambúrguer de cogumelos, queijo brie, azeite trufado e rúcula.", preco: "50,00" },
+]
+
+
+function exibirLista() {
+    for (var i = 0; i < listaProdutos.length; i++) {
+        document.getElementById("produtos").innerHTML+=`<li class="card">
+            <div  class="divphoto"><img class="imgproduct" src="${listaProdutos[i].img}"></div>    
+            <h3  class="nomeproduto">${listaProdutos[i].titulo}</h3>
+            <p class="descricao">${listaProdutos[i].descricao} </p>
+            <p class="descricao-preco" > <strong> R$${listaProdutos[i].preco} </strong></p>
+            <button id="botao" class="hamburguerInfo--addButton" onclick="adicionarNoCarrinho(${i})" >Adicionar ao Carrinho</button>
+            </li>
+            `
+    }
+}
+
+exibirLista();
 
 // Criar a variável modalKey global
 let modalKey = 0;
@@ -108,7 +105,7 @@ const adicionarNoCarrinho = (index) =>  {
     // Verificar se já existe o item no carrinho
     let key = cart.findIndex((item) => item.id === identificador);
     
-    if (key > -1) {
+    if (key > 1) {
         // Se já existir, aumentar a quantidade
         cart[key].qt += quantHamburguer;
     } else {
@@ -131,6 +128,21 @@ const adicionarNoCarrinho = (index) =>  {
     abrirCarrinho();
     atualizarCarrinho();
 };
+// function adicionarNoCarrinho(event) {
+//   const index = event.target.dataset.index;
+//   const produto = listaProdutos[index];
+//   const identificador = produto.id;
+//   const price = parseFloat(produto.preco);
+//   let itemCarrinho = cart.find(item => item.identificador === identificador);
+//   if (itemCarrinho) {
+//       itemCarrinho.qt += quantHamburguer;
+//   } else {
+//       cart.push({ identificador, id: produto.id, size, qt: quantHamburguer, price });
+//   }
+//   atualizarCarrinho();
+//   abrirCarrinho();
+// }
+
 
 // const adicionarNoCarrinho = () => {
 //     // console.log(seleciona('.hamburguerInfo--addButton'))
